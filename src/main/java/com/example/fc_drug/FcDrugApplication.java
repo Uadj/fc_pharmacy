be72b0cd015646e70e51b9d1fdbf23a5a860c813
DGW <ds257873@naver.com>
2023-10-08 08:07:47 +0900

    Initial commit

---
+++ src/main/java/com/example/fc_drug/FcDrugApplication.java
@@ -0,0 +1,13 @@
+package com.example.fc_drug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcDrugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcDrugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fc_drug/FcDrugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fc_drug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcDrugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
